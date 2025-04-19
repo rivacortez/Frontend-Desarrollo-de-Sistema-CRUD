800edb9a1b200121405acb1ec24acad7c6878161
rivacortezf2006 <u202215313@upc.edu.pe>
2025-04-19 14:22:37 -0500

    chore(config): add api config env

---
+++ src/config/api.ts
@@ -0,0 +1,9 @@
+
+const API_BASE_URL = 'http://127.0.0.1:8000/api';
+
+export const API_ENDPOINTS = {
+  TABLES: `${API_BASE_URL}/tables`,
+  CUSTOMERS: `${API_BASE_URL}/customers`,
+  RESERVATIONS: `${API_BASE_URL}/reservations`
+};
+
